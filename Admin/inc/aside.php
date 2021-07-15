7016fab54e8eef045f60938e26feb463b8759f7c
Ernest Kyei Nkrumah <Mishrule1@gmail.com>
2021-07-15 23:13:53 +0000

    project setup

---
+++ Admin/inc/aside.php
@@ -0,0 +1,475 @@
+<?php 
+    echo '
+    <div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <img src="assets/images/logo.png" alt="" srcset="">
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+
+                <li class="sidebar-title">MANAGEMENT</li>
+
+                <li class="sidebar-item active ">
+                    <a href="index.php" class="sidebar-link">
+                        <i data-feather="home" width="20"></i>
+                        <span>Dashboard</span>
+                    </a>
+
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="triangle" width="20"></i>
+                        <span>Property</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="create_property.php">Create Property</a>
+                        </li>
+
+                        <li>
+                            <a href="manage_property.php">Manage Property</a>
+                        </li>
+
+                        <li>
+                            <a href="view_property.php">View Property</a>
+                        </li>
+<!--
+                        <li>
+                            <a href="component-buttons.html">Buttons</a>
+                        </li>
+
+                        <li>
+                            <a href="component-card.html">Card</a>
+                        </li>
+
+                        <li>
+                            <a href="component-carousel.html">Carousel</a>
+                        </li>
+
+                        <li>
+                            <a href="component-dropdowns.html">Dropdowns</a>
+                        </li>
+
+                        <li>
+                            <a href="component-list-group.html">List Group</a>
+                        </li>
+
+                        <li>
+                            <a href="component-modal.html">Modal</a>
+                        </li>
+
+                        <li>
+                            <a href="component-navs.html">Navs</a>
+                        </li>
+
+                        <li>
+                            <a href="component-pagination.html">Pagination</a>
+                        </li>
+
+                        <li>
+                            <a href="component-progress.html">Progress</a>
+                        </li>
+
+                        <li>
+                            <a href="component-spinners.html">Spinners</a>
+                        </li>
+
+                        <li>
+                            <a href="component-tooltips.html">Tooltips</a>
+                        </li>
+-->
+                    </ul>
+
+                </li>
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="triangle" width="20"></i>
+                        <span>Agents</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="agent.php">Add Agent</a>
+                        </li>
+
+                        <li>
+                            <a href="manage_agent.php">Manage Agent</a>
+                        </li>
+
+                        <li>
+                            <a href="agent_list.php">Agent List</a>
+                        </li>
+<!--
+                        <li>
+                            <a href="component-buttons.html">Buttons</a>
+                        </li>
+
+                        <li>
+                            <a href="component-card.html">Card</a>
+                        </li>
+
+                        <li>
+                            <a href="component-carousel.html">Carousel</a>
+                        </li>
+
+                        <li>
+                            <a href="component-dropdowns.html">Dropdowns</a>
+                        </li>
+
+                        <li>
+                            <a href="component-list-group.html">List Group</a>
+                        </li>
+
+                        <li>
+                            <a href="component-modal.html">Modal</a>
+                        </li>
+
+                        <li>
+                            <a href="component-navs.html">Navs</a>
+                        </li>
+
+                        <li>
+                            <a href="component-pagination.html">Pagination</a>
+                        </li>
+
+                        <li>
+                            <a href="component-progress.html">Progress</a>
+                        </li>
+
+                        <li>
+                            <a href="component-spinners.html">Spinners</a>
+                        </li>
+
+                        <li>
+                            <a href="component-tooltips.html">Tooltips</a>
+                        </li>
+-->
+                    </ul>
+
+                </li>
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="triangle" width="20"></i>
+                        <span>LandLords</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="create_landlord.php">Add Landlords</a>
+                        </li>
+
+                        <li>
+                            <a href="manage_landlord.php">Manage Landlord</a>
+                        </li>
+<!--
+                        <li>
+                            <a href="component-breadcrumb.html">Breadcrumb</a>
+                        </li>
+
+                        <li>
+                            <a href="component-buttons.html">Buttons</a>
+                        </li>
+
+                        <li>
+                            <a href="component-card.html">Card</a>
+                        </li>
+
+                        <li>
+                            <a href="component-carousel.html">Carousel</a>
+                        </li>
+
+                        <li>
+                            <a href="component-dropdowns.html">Dropdowns</a>
+                        </li>
+
+                        <li>
+                            <a href="component-list-group.html">List Group</a>
+                        </li>
+
+                        <li>
+                            <a href="component-modal.html">Modal</a>
+                        </li>
+
+                        <li>
+                            <a href="component-navs.html">Navs</a>
+                        </li>
+
+                        <li>
+                            <a href="component-pagination.html">Pagination</a>
+                        </li>
+
+                        <li>
+                            <a href="component-progress.html">Progress</a>
+                        </li>
+
+                        <li>
+                            <a href="component-spinners.html">Spinners</a>
+                        </li>
+
+                        <li>
+                            <a href="component-tooltips.html">Tooltips</a>
+                        </li>
+-->
+                    </ul>
+
+                </li>
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="triangle" width="20"></i>
+                        <span>Payments</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="make_payment.php">Make Payment</a>
+                        </li>
+
+                        <li>
+                            <a href="view_payment.php">View Payment</a>
+                        </li>
+<!--
+                        <li>
+                            <a href="component-breadcrumb.html">Breadcrumb</a>
+                        </li>
+
+                        <li>
+                            <a href="component-buttons.html">Buttons</a>
+                        </li>
+
+                        <li>
+                            <a href="component-card.html">Card</a>
+                        </li>
+
+                        <li>
+                            <a href="component-carousel.html">Carousel</a>
+                        </li>
+
+                        <li>
+                            <a href="component-dropdowns.html">Dropdowns</a>
+                        </li>
+
+                        <li>
+                            <a href="component-list-group.html">List Group</a>
+                        </li>
+
+                        <li>
+                            <a href="component-modal.html">Modal</a>
+                        </li>
+
+                        <li>
+                            <a href="component-navs.html">Navs</a>
+                        </li>
+
+                        <li>
+                            <a href="component-pagination.html">Pagination</a>
+                        </li>
+
+                        <li>
+                            <a href="component-progress.html">Progress</a>
+                        </li>
+
+                        <li>
+                            <a href="component-spinners.html">Spinners</a>
+                        </li>
+
+                        <li>
+                            <a href="component-tooltips.html">Tooltips</a>
+                        </li>
+-->
+                    </ul>
+
+                </li>
+<!--
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="briefcase" width="20"></i>
+                        <span>Agents</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="component-extra-avatar.html">Add Agents</a>
+                        </li>
+
+                        <li>
+                            <a href="component-extra-divider.html">View Agent</a>
+                        </li>
+
+                    </ul>
+
+                </li>
+-->
+
+                <li class="sidebar-title">ACTIVITIES</li>
+
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="file-text" width="20"></i>
+                        <span>Testimonials</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="add_testimonial.php">Add Testimonial</a>
+                        </li>
+
+                        <li>
+                            <a href="manage_testimonial.php">Manage Testimonial</a>
+                        </li>
+<!--
+                        <li>
+                            <a href="form-element-select.html">Select</a>
+                        </li>
+
+                        <li>
+                            <a href="form-element-radio.html">Radio</a>
+                        </li>
+
+                        <li>
+                            <a href="form-element-checkbox.html">Checkbox</a>
+                        </li>
+
+                        <li>
+                            <a href="form-element-textarea.html">Textarea</a>
+                        </li>
+-->
+                    </ul>
+
+                </li>
+
+
+
+
+                <li class="sidebar-item  ">
+                    <a href="news.php" class="sidebar-link">
+                        <i data-feather="layout" width="20"></i>
+                        <span>News</span>
+                    </a>
+
+                </li>
+<!--
+                <li class="sidebar-item  ">
+                    <a href="form-editor.html" class="sidebar-link">
+                        <i data-feather="layers" width="20"></i>
+                        <span>Form Editor</span>
+                    </a>
+
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="table.html" class="sidebar-link">
+                        <i data-feather="grid" width="20"></i>
+                        <span>Table</span>
+                    </a>
+
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="table-datatable.html" class="sidebar-link">
+                        <i data-feather="file-plus" width="20"></i>
+                        <span>Datatable</span>
+                    </a>
+
+                </li>
+-->
+                <li class="sidebar-title">AUTH USERS</li>
+
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="user" width="20"></i>
+                        <span>Authentication</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="users.php">Users</a>
+                        </li>
+
+                        <li>
+                            <a href="manage_user.php>Manage Users</a>
+                        </li>
+
+                      <!--  <li>
+                            <a href="Logout.php">Logout</a>
+                        </li> -->
+
+                    </ul>
+
+                </li>
+
+
+                <li class="sidebar-title">SETTINGS</li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="user" width="20"></i>
+                        <span>App Settings</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="auth-login.html">Users</a>
+                        </li>
+
+                        <li>
+                            <a href="auth-register.html">Manage Users</a>
+                        </li>
+
+                      <!--  <li>
+                            <a href="auth-forgot-password.html">Forgot Password</a>
+                        </li> -->
+
+                    </ul>
+
+                </li>
+
+
+
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i data-feather="alert-circle" width="20"></i>
+                        <span>Errors</span>
+                    </a>
+
+                    <ul class="submenu ">
+
+                        <li>
+                            <a href="error-403.html">403</a>
+                        </li>
+
+                        <li>
+                            <a href="error-404.html">404</a>
+                        </li>
+
+                        <li>
+                            <a href="error-500.html">500</a>
+                        </li>
+
+                    </ul>
+
+                </li>
+
+
+
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
+    ';
+?>
